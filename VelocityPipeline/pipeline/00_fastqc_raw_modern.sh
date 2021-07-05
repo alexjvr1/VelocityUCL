5b25ddf13bdf4bc218ea6ba8887e728a5a0d8652
krl516 <79085449+krl516@users.noreply.github.com>
2021-07-05 15:14:56 +0100

    Update 00_fastqc_raw_modern.sh

--- VelocityPipeline/pipeline/00_fastqc_raw_modern.sh
+++ VelocityPipeline/pipeline/00_fastqc_raw_modern.sh
@@ -2,9 +2,10 @@
 ##################################
 # Alexandra Jansen van Rensburg
 # alexjvr@gmail.com
-# Last modified 24/10/2018 14:28
+# Last modified 05/07/2021 15:14
 ##################################
+#v1 - modified path to wrapper script
 
 # Creates submission script to check quality of demultiplexed raw reads using fastqc
 
-/panfs/panasas01/bisc/aj18951/bristol-velocity/AJvR_VelocityPipeline/wrapper/00_parallel_fastqc_bcp3.sh -i 00_raw_reads_museum/ -o /00_raw_reads_museum/ -n 1 -t 8 -m 4;
+/SAN/ugi/LepGenomics/VelocityPipeline/wrapper/00_parallel_fastqc_bcp3.sh -i 00_raw_reads_museum/ -o /00_raw_reads_museum/ -n 1 -t 8 -m 4;
