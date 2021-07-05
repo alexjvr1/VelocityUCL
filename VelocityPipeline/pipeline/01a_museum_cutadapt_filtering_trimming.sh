cb03d3ee1803e78a40b0819ad08e2834f6177aa7
krl516 <79085449+krl516@users.noreply.github.com>
2021-07-05 15:19:41 +0100

    Update 01a_museum_cutadapt_filtering_trimming.sh

--- VelocityPipeline/pipeline/01a_museum_cutadapt_filtering_trimming.sh
+++ VelocityPipeline/pipeline/01a_museum_cutadapt_filtering_trimming.sh
@@ -4,6 +4,7 @@
 # alexjvr@gmail.com
 # Last modified 24/10/2018 14:28
 ##################################
+#v1 - modified path to tools script
 
 # Creates submission script to use cutadapt to remove adapters from demultiplexed Illumina libraries. 
 # Poly-A and -T filters have been removed as we decided that these would be dropped due to mapping quality by bwa mem. 
@@ -13,7 +14,7 @@
 
 
 
-/panfs/panasas01/bisc/aj18951/bristol-velocity/AJvR_VelocityPipeline/tools/01a_parallel_cutadapt_bluecp3.sh \
+/SAN/ugi/LepGenomics/VelocityPipeline/tools/01a_parallel_cutadapt_bluecp3.sh \
 -i 00_raw_reads_museum1 \
 -o 01a_museum_cutadapt_reads -n 1 -t 8 -m 8 -ph 33 \
 -fwad1 AGATCGGAAGAGCACACGTCTGAACTCCAGTC -rvad1 AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
