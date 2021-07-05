4ada43b66bbaa1bd9e6969e87c2444c16a146a4c
alexjvr1 <alexjvr1@users.noreply.github.com>
2021-07-05 12:27:03 +0100

    Create 04_fst_scans_etc_SNP_based.sh

---
+++ VelocityPipeline/wrapper/04_fst_scans_etc_SNP_based.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -N 03b_Summary  ##job name
+#PBS -l nodes=1:ppn=1  #nr of nodes and processors per node
+#PBS -l mem=16gb #RAM
+#PBS -l walltime=8:00:00 ##wall time.  
+#PBS -j oe  #concatenates error and output files (with prefix job1)
+
+
+#run job in working directory
+cd $PBS_O_WORKDIR
+
+
+~/bristol-velocity/AJvR_VelocityPipeline/tools/popgenstats.pl -i 04a_Fst_scans_and_other_scatistics_indir/G1.subset.museum.modern.bcf -p 04a_Fst_scans_and_other_scatistics_indir/Thymelicus_acteon_population_file.dsv -o 04b_Fst_scans_and_other_scatistics_outdir/Fst_outfile.dsv -n 1 -ms 0.5 -msp 0.4 -mf 0 -pr 1 -am 1 -fm 0;
