93ea2645e3c4c40096a34145bd92c0238b1c2b78
krl516 <79085449+krl516@users.noreply.github.com>
2021-07-05 15:51:23 +0100

    Update 01b_modern_trimmomatic_filtering_trimming.sh

--- VelocityPipeline/pipeline/01b_modern_trimmomatic_filtering_trimming.sh
+++ VelocityPipeline/pipeline/01b_modern_trimmomatic_filtering_trimming.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 ##################################
 # Alexandra Jansen van Rensburg
-# Last Modified 24/10/2018
+# Last Modified 05/07/2021
 ##################################
+#v1 -modified path to wrapper script
 
 # Generates submission script for second part of the trimming process using Trimmomatic. 
 # Romain found that the Cutadapt step (01a) does not remove all of the adapter sequence. 
 
 
-/panfs/panasas01/bisc/aj18951/bristol-velocity/AJvR_VelocityPipeline/wrapper/parallel_trimmomatic_bluecp3.sh \
+/SAN/ugi/LepGenomics/VelocityPipeline/wrapper/parallel_trimmomatic_bluecp3.sh \
 -i 01a_modern_cutadapt_filtered_reads \
 -o 01b_modern_trimmomatic_filtered_reads \
 -n 1 -t 8 -m 4 -ph 33 -c 150 -hc 0 \
