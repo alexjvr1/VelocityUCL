73ebeb108b504a56e60901dc754ae6ec5ee7a0c0
krl516 <79085449+krl516@users.noreply.github.com>
2021-07-08 14:41:18 +0100

    Create 00_fastqc_raw_museum2.sh

---
+++ KaitlynNotes/00_fastqc_raw_museum2.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+##################################
+# Kaitlyn Louth supervised by Alexandra Jansen van Rensburg
+# alexjvr@gmail.com
+# Last modified 08/07/2021 14:40
+##################################
+
+# Creates submission script to check quality of demultiplexed raw reads using fastqc
+
+/SAN/ugi/LepGenomics/VelocityPipeline/wrapper/00_parallel_fastqc_UCL.sh
+-i 00_raw_reads_museum2/
+-o /00_raw_reads_museum2/
+-n 1 -t 8 -m 4 -v 4;
