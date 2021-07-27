6467def112f337f9270298415d728bf2cff860a3
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-07-27 13:54:17 +0100

    Update 02b.0_AddRG_MODC.sh

--- ATLAS/Scripts/02b.0_AddRG_MODC.sh
+++ ATLAS/Scripts/02b.0_AddRG_MODC.sh
@@ -22,11 +22,15 @@ SPECIES=E3_Aphantopus_hyperantus
 REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
 INPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_modern
 OUTPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_modern
-TAIL="R1.repaired.fastq.gz.repaired.merged.fastq.gz.bam"
+TAIL=`ls 02a_mapped_modern/*bam | awk -F "/" '{print $NF}' | awk '{print substr($0,14)}' | head -n1`
+
+
 
 #Set up ARRAY job
-ls 02a_mapped_modern/*bam | awk -F "/" '{print $NF}' | awk -F "." '{print $1}' > modc.names 
+ls 02a_mapped_modern/*bam | awk -F "/" '{print $NF}' | awk -F "_" '{print $1}' > modc.names 
 NAME=$(sed "${SGE_TASK_ID}q;d" modc.names)
+
+
 
 
 ##Add readgroups
