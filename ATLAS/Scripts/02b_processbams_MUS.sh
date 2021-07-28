6d95b2d8b757d0d3bfe6e39c14a9839018bfb561
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-07-28 13:54:18 +0100

    Create 02b_processbams_MUS.sh

---
+++ ATLAS/Scripts/02b_processbams_MUS.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N E3.MUS.processbams  ##job name
+#$ -l tmem=32G #RAM
+#$ -l h_vmem=32G #enforced limit on shell memory usage
+#$ -l h_rt=1:00:00 ##wall time.  
+#$ -j y  #concatenates error and output files (with prefix job1)
+
+###Script to process mapped bam files in series
+##1. Add RG information
+##2. Remove duplicates
+##3. Local realignment with GATK3.8
+##4. Validate sam file
+####################################
+
+#Run on working directory
+cd $SGE_O_WORKDIR 
+
+#Call software
+export PATH=/share/apps/java/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/java/lib:$LD_LIBRARY_PATH
+PICARD=/share/apps/genomics/picard-2.20.3/bin/picard.jar
+
+
+#Define variables
+SHAREDFOLDER=/SAN/ugi/LepGenomics
+SPECIES=E3_Aphantopus_hyperantus
+REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
+INPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_museum
+OUTPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_museum
+TAIL1=`ls 02a_mapped_museum/*bam | awk -F "/" '{print $NF}' | awk '{print substr($0,14)}' | head -n 1`
+TAIL2="RG.bam"
+
+
+############################
+#1. Add ReadGroups
+############################
+
+#Create input file
+ls $INPUT/*bam | awk -F "/" '{print $NF}' | awk -F "_" '{print $1}' > mus.names 
+
+#Write command to log file
+
+echo "while read NAME1 <&1; do java -jar $PICARD AddOrReplaceReadGroups \
+       I=$INPUT/$NAME1.$TAIL1 \
+       O=$OUTPUT/$NAME1.RG.bam \
+       RGID=E3mus \
+       RGLB=museum0204 \
+       RGPL=ILLUMINA \
+       RGPU=unit1 \
+       RGSM=$NAME1; done 1<mus.names" >> 02b.0_AddRG.log
+
+
+time while reads NAME1 <&1; do java -jar $PICARD AddOrReplaceReadGroups \
+       I=$INPUT/$NAME1$TAIL1 \
+       O=$OUTPUT/$NAME1.RG.bam \
+       RGID=E3mus \
+       RGLB=museum0204 \
+       RGPL=ILLUMINA \
+       RGPU=unit1 \
+       RGSM=$NAME1; done 1<mus.names 
+
+
+
+#############################
+#2. Mark Duplicates
+#############################
+
+#Create list of bam files to process
+ls $INPUT/*RG.bam | awk -F "." '{print $1}' >> modc.names
+
+#Write command to log file
+echo "while read NAME2 <&2; do 
+java -jar $PICARD MarkDuplicates \
+INPUT=$INPUT/$NAME2.$TAIL2 \
+OUTPUT=$OUTPUT/$NAME2.rmdup.bam \
+METRICS_FILE=$OUTPUT/$NAME2.dup.txt \
+REMOVE_DUPLICATES=true \
+VALIDATION_STRINGENCY=SILENT \
+CREATE_INDEX=true; done 2<mus.names" >> 02b.ProcessBams.log
+
+#Run command
+time while read NAM1 <&2; do java -jar $PICARD MarkDuplicates \
+INPUT=$INPUT/$NAME2.$TAIL2 \
+OUTPUT=$OUTPUT/$NAME2.rmdup.bam \
+METRICS_FILE=$OUTPUT/$NAME2.dup.txt \
+REMOVE_DUPLICATES=true \
+VALIDATION_STRINGENCY=SILENT \
+CREATE_INDEX=true; done 2<mus.names
+
+
+
+#############################
+#3. Local realignment
+#############################
+
+#We import an old version of GATK and an older version of java for this step
+export PATH=/share/apps/jdk1.8.0_131/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/jdk1.8.0_131/lib:$LD_LIBRARY_PATH
+GenomeAnalysisTK=/share/apps/genomics/GenomeAnalysisTK-3.8.1.0/GenomeAnalysisTK.jar
+
+#create input file
+ls $INPUT/*rmdup.bam |awk -F "/" '{print $NF}' | awk -F "." '{print $1}' > mus.names 
+
+# Identify targets to realign
+time while read NAME3 <$3; do java -jar $GenomeAnalysisTK -T RealignerTargetCreator \
+-R $REF \
+-o $OUTPUT/$NAME3..intervals \
+-I $INPUT/$NAME3.rmdup.bam; done 3<mus.names
+
+
+# use IndelRealigner to realign the regions found in the RealignerTargetCreator step
+while read NAME3 <&3; do java -jar $GenomeAnalysisTK -T IndelRealigner \
+-R $REF \
+-targetIntervals $INPUT/$NAME3.intervals \
+-I $INPUT/$NAME3.rmdup.bam \
+-o $OUTPUT/$NAME3.realn.bam; done 3<mus.names
+
+
+
+#############################
+#4. Validate bam file
+#############################
+
+#Import the newest version of java again
+export PATH=/share/apps/java/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/java/lib:$LD_LIBRARY_PATH
+PICARD=/share/apps/genomics/picard-2.20.3/bin/picard.jar
+
+#Define variables
+TAIL4="realn.bam"
+
+#create input file
+ls $INPUT/*realn.bam |awk -F "/" '{print $NF}' | awk -F "." '{print $1}' > mus.names
+
+#Write command to log file
+echo "while read NAME4 <&4; do java -jar $PICARD ValidateSamFile \
+INPUT=$INPUT/$NAME4.$TAIL4 \
+OUTPUT=$OUTPUT/$NAME4.validatesam
+MODE=SUMMARY; done 4<mus.names" >> 02b.ProcessBams.log
+
+#Run validatesamfile
+time while read NAME4 <&4; do java -jar $PICARD ValidateSamFile \
+INPUT=$INPUT/$NAME4.$TAIL4 \
+OUTPUT=$OUTPUT/$NAME4.validatesam \
+MODE=SUMMARY; done 4<mus.names
