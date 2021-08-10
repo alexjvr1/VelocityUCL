994ae4a7498369983a2b8c19160ae13b1d0249fd
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-08-10 17:08:35 +0100

    Create Figure_fvec.sh

---
+++ diploSHIC/Figure_fvec.sh
@@ -0,0 +1,16 @@
+# Plot fvec file
+
+Plot graphic for feature vectors
+
+
+R colour palette ideas [here](https://www.nceas.ucsb.edu/sites/default/files/2020-04/colorPaletteCheatsheet.pdf). 
+
+I'll use colorRamps::matlab.like
+
+
+
+
+
+
+
+
