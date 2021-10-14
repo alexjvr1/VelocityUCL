42ea31a9d6fba03ae8d66ac9b2f959e5c55c664e
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-10-14 12:35:47 +0100

    Create 02b.3_ValidateSamFile_MODC.sh

---
+++ Scripts/02b.3_ValidateSamFile_MODC.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N E3.MODC.ValidateSamFile  ##job name
+#$ -l tmem=16G #RAM
+#$ -l h_vmem=16G #enforced limit on shell memory usage
+#$ -l h_rt=1:00:00 ##wall time.  
+#$ -j y  #concatenates error and output files (with prefix job1)
+#$ -t 1-38
+
+
+#Run on working directory
+cd $SGE_O_WORKDIR 
+
+#Call software
+export PATH=/share/apps/java/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/java/lib:$LD_LIBRARY_PATH
+PICARD=/share/apps/genomics/picard-2.20.3/bin/picard.jar
+
+
+#Define variables
+SHAREDFOLDER=/SAN/ugi/LepGenomics
+SPECIES=E3_Aphantopus_hyperantus
+REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
+INPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_modern
+OUTPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_modern
+TAIL="realn.bam"
+
+#Set up ARRAY job
+#ls *bam | awk -F "." '{print $1}' >> modc.names
+NAME=$(sed "${SGE_TASK_ID}q;d" modc.names)
+
+echo "java -jar $PICARD ValidateSamFile \
+INPUT=$INPUT/${NAME}.realn.bam \
+OUTPUT=$OUTPUT/${NAME}.validatesam
+MODE=SUMMARY" >> 02b.3_ValidateSamFile.log
+
+
+time java -jar $PICARD ValidateSamFile \
+INPUT=$INPUT/${NAME}.realn.bam \
+OUTPUT=$OUTPUT/${NAME}.validatesam \
+MODE=SUMMARY
