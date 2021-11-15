df64b6b2e7ccc47cb5bd7b743aeb1d3591b02738
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-11-15 15:24:35 +0000

    Create 01a_Trimmomatic.sh

---
+++ Scripts/01a_Trimmomatic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##################################
+# Alexandra Jansen van Rensburg
+# Last Modified 05/07/2021
+##################################
+#v1 -modified path to wrapper script
+
+# Generates submission script for first part of the trimming process using Trimmomatic. 
+
+/SAN/ugi/LepGenomics/VelocityPipeline/wrapper/parallel_trimmomatic.sh \
+-i 00_raw_reads_museum/TEST \
+-o TrimmomaticTest \
+-n 1 -t 8 -m 4 -l 16 -ph 33 -c 150 -hc 0 \
+-ad /SAN/ugi/LepGenomics/Software/Trimmomatic-0.39/adapters/TruSeq3-PE-2.fa \
+-illclip '2:30:8:1:true' -le 20 -tr 20 -sw '4:20' -minl 20 -avg 20;
