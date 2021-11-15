001703d7d07e4832e5bdd75897b7f985fc1f9d95
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-11-15 12:46:03 +0000

    Create 00_fastqc_raw_museum.sh

---
+++ Scripts/00_fastqc_raw_museum.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##################################
+# Alexandra Jansen van Rensburg
+# alexjvr@gmail.com
+# Last modified 06/07/2021 11:20
+##################################
+
+# Creates submission script to check quality of demultiplexed raw reads using fastqc
+
+
+SHAREDFOLDER=/SAN/ugi/LepGenomics
+SCRIPTS=VelocityPipeline/wrapper
+SPECIES=C3_Aricia_agestis
+INFILE=00_raw_reads_museum
+OUTFILE=00_raw_reads_museum
+
+
+$SHAREDFOLDER/$SCRIPTS/00_parallel_fastqc_UCL.sh \
+-i $SHAREDFOLDER/$SPECIES/$INFILE \
+-o $SHAREFOLDER/$SPECIES/$OUTFILE \
+-t 1 -m 4 -v 4;
