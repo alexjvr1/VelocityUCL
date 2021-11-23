cf9144f9ca6f838021dc1c55ceb926b8e370af54
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-11-23 08:58:26 +0000

    Update 00_parallel_fastqc_bcp3.sh
    changed INPUT_TMPDIR to =$INDIR
    We don't need a tmpdir on the UCL CS

--- VelocityPipeline/wrapper/00_parallel_fastqc_bcp3.sh
+++ VelocityPipeline/wrapper/00_parallel_fastqc_bcp3.sh
@@ -2,7 +2,7 @@
 
 # (c) Romain Villoutreix
 # romain.villoutreix@gmail.com
-# Last modified: 06/10/2018 
+# Last modified: 06/07/2021 
 
 # Description:
 # Given an input directory with raw fastq files (gzip and bzip2 compression allowed)
@@ -14,8 +14,10 @@
 #Email forwarding not active on bluecrystalp3
 #Look into core options
 
-VERSION='1.1-2018.10.06'
+VERSION='1.1-2021.07.06'
 CMD="/share/apps/genomics/FastQC-0.11.8/fastqc"
+
+
 
 # Default values for optional variables
 EXTRA=''
@@ -23,6 +25,7 @@ NOEXTRACT='yes'
 NCORES=1
 HRS=8
 MEM=4
+VMEM=4
 
 function author {
 	echo
@@ -44,6 +47,7 @@ function usage {
 	echo "      -n <number of processors> => processors per library (optional, default=$NCORES)"
 	echo "      -t <allocated time> => Allocated time (in hours) for each analysis (optional: default=$HRS)"
 	echo "      -m <allocated memory> => Allocated memory (in gigabytes) for each analysis (optional: default=$MEM)"
+	echo "	    -v <enforced memory limit> => Enforced memory (in gigabytes) (optional: default=$VMEM)"
 	echo "      -h => show this help"
 	echo ""
 	echo "  Example:"
@@ -76,6 +80,9 @@ then
 			-m)	shift
 				MEM=$1
 				;;
+			-v)	shift
+				VMEM=$1
+				;;
 			*)	echo 
 				echo "ERROR - Invalid option: $1"
 				echo
@@ -91,14 +98,7 @@ fi
 N=$(find $INDIR -maxdepth 1 -name "*.fastq*" | wc -l | cut -f1 -d" ")
 
 TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-TMPDIR="parallel_fastqc_"$TIMESTAMP"_"$RANDOM
 
-INPUT_LOCALDIR="/local/$TMPDIR""_in"
-
-if [[ $QUEUE == "popgenom" || $QUEUE == "molecol" ]];
-then
-	INPUT_LOCALDIR="/local/tmp/$TMPDIR""_in"
-fi
 
 SMSJOB="$OUTDIR/parallel_fastqc.$TIMESTAMP.smsjob.sh"
 LOG="$OUTDIR/parallel_fastqc.$TIMESTAMP.smsjob.log"
@@ -107,51 +107,52 @@ LOG="$OUTDIR/parallel_fastqc.$TIMESTAMP.smsjob.log"
 mkdir -p $OUTDIR
 echo '#!/bin/bash' > $SMSJOB
 
-# SGE OPTIONS
+# UCL server OPTIONS
 # -----------------------------------------
-echo '#PBS -l walltime='$HRS':00:00' >> $SMSJOB
-echo '#PBS -l mem='$MEM'gb' >> $SMSJOB
+echo '#$ -S /bin/bash' >> $SMSJOB
+echo '#$ -l h_rt='$HRS':00:00' >> $SMSJOB
+echo '#$ -l tmem='$MEM'G' >> $SMSJOB
+echo '#$ -l h_vmem='$VMEM'G' >> $SMSJOB
 ####TOFIX this: number of core asked per job
 #if (($NCORES > 1 ));
 #then
 #	echo '#$ -pe openmp '$NCORES >> $SMSJOB
 #fi
-echo '#PBS -t 1-'$N >> $SMSJOB
-echo '#PBS -j oe' >> $SMSJOB
-echo '#PBS -o '$LOG >> $SMSJOB
+echo '#$ -t 1-'$N >> $SMSJOB
+echo '#$ -j y' >> $SMSJOB
+#echo '#$ -o '$LOG >> $SMSJOB
 # -----------------------------------------
+
+
+
 
 cat >> $SMSJOB <<EOF
 
-module load apps/fastqc-0.11.5 
+#Add Java to PATH
+export PATH=/share/apps/java/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/java/lib:$LD_LIBRARY_PATH
 
 INDIR=$INDIR
 OUTDIR=$OUTDIR
 FQFILES=(\$INDIR/*.fastq*)
-#INDEX=\$((SGE_TASK_ID-1))
-INDEX=\$((PBS_ARRAYID-1))
+INDEX=\$((SGE_TASK_ID-1))
 FQ=\${FQFILES[\$INDEX]}
 EOF
 
 cat >> $SMSJOB <<EOF
-
-INPUT_TMPDIR=$INPUT_LOCALDIR
-
+INPUT_TMPDIR=$INDIR
 # Create temporary local directories
 if [ ! -e \$INPUT_TMPDIR ];
 then
 	mkdir -p \$INPUT_TMPDIR
 fi
-
 # copy to local temporary directory
 cp \$FQ \$INPUT_TMPDIR/
 FQ=\$(basename \$FQ)
 EOF
 
 cat >> $SMSJOB <<EOF
-
 LOG="\$OUTDIR/"\${FQ%%.*}".log"
-
 echo "fastQC \$FQ file..." > \$LOG
 echo >> \$LOG 
 echo "CMD: " >> \$LOG
@@ -166,9 +167,7 @@ echo "--no-extract \\\" >> \$LOG
 echo >> \$LOG
 echo "---------------------------------------------------" >> \$LOG
 echo >> \$LOG
-
 cd \$INPUT_TMPDIR
-
 $CMD \\
 \$FQ \\
 EOF
@@ -178,17 +177,14 @@ cat >> $SMSJOB <<EOF
 -t $NCORES \\
 --no-extract \\
 >> \$LOG 2>&1
-
 #remove temporary files
 rm -f \$INPUT_TMPDIR/\$FQ
-
-
 EOF
 
 chmod +x $SMSJOB
 
 
-echo "Command to submit the job to bluecrystal ($QUEUE queue):"
+echo "Command to submit the job to UCL server ($QUEUE queue):"
 echo
 echo "qsub $SMSJOB"
 echo
