902f4ee581df52da4aa6d57697d54131c3f5364d
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-11-26 12:34:00 +0000

    Update Index.sh

--- Scripts/Index.sh
+++ Scripts/Index.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -N E3.MODC.Index  ##job name
+#$ -N E3.mus.index  ##job name
 #$ -l tmem=16G #RAM
-#$ -l h_vmem=16G #enforced limit on shell memory usage
-#$ -l h_rt=1:00:00 ##wall time.
+#$ -l h_vmem=16G #enforced limit on memory shell usage
+#$ -l h_rt=1:00:00 ##wall time.  
 #$ -j y  #concatenates error and output files (with prefix job1)
+##$ -t 1-48
 
-#Script to index a list of bamfiles specified in an input textfile
-
-#Run on working directory
+#run job in working directory
 cd $SGE_O_WORKDIR 
 
 
-#Path to software
-samtools=/share/apps/genomics/samtools-1.9/bin/samtools
+##Software
+export PATH=/share/apps/genomics/samtools-1.9/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/genomics/samtools-1.9/lib:$LD_LIBRARY_PATH
 
-#Define variables
-SHAREDFOLDER=/SAN/ugi/LepGenomics
-SPECIES=E3_Aphantopus_hyperantus
-FOLDER=E3_SubsetTests
-REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
-INPUT=$SHAREDFOLDER/$FOLDER/02a_mapped_modern_Downsampled/modc.Downsampled.merged.bamlist
-OUTPUT=$SHAREDFOLDER/$FOLDER/02a_mapped_modern_Downsampled
-TASK=theta
+for i in $(ls *bam); do samtools index $i; done
 
-
-#Run analysis
-while IFS=  read -r line; do $samtools index $line; done < $INPUT
