5ae08d33c93b1a6a917b9e2c9cec317e394374bd
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-11-26 12:14:20 +0000

    Update 02a_MapwithBWAmem.ARRAY_MODC.sh

--- Scripts/02a_MapwithBWAmem.ARRAY_MODC.sh
+++ Scripts/02a_MapwithBWAmem.ARRAY_MODC.sh
@@ -22,10 +22,11 @@ SPECIES=E3_Aphantopus_hyperantus
 REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
 INPUT=$SHAREDFOLDER/$SPECIES/01b_AdapterRemoval_MODC.unmerged
 OUTPUT=$SHAREDFOLDER/$SPECIES/02a_mapped_MODC
+TAIL1=pair1.truncated
+TAIL2=pair2.truncated
 
 ##Define ARRAY names
-NAME1=$(sed "${SGE_TASK_ID}q;d" R1.modc.names)
-NAME2=$(sed "${SGE_TASK_ID}q;d" R2.modc.names)
+NAME=$(sed "${SGE_TASK_ID}q;d" modc.names)
 
 
 ##Map using array
@@ -33,5 +34,5 @@ NAME2=$(sed "${SGE_TASK_ID}q;d" R2.modc.names)
 sample_name=`echo ${NAME1} | awk -F "." '{print $1}'`
 echo "[mapping running for] $sample_name"
 printf "\n"
-echo "time $BWA mem $REF $INPUT/${NAME1} $INPUT/${NAME2}| samtools sort -o  $OUTPUT/${NAME1}.bam" >> map_modc.log
-time $BWA mem $REF $INPUT/${NAME1} $INPUT/${NAME2} | samtools sort -o  $OUTPUT/${NAME1}.bam
+echo "time $BWA mem $REF $INPUT/${NAME}.$TAIL1 $INPUT/${NAME}.$TAIL2| samtools sort -o  $OUTPUT/${NAME}.bam" >> map_modc.log
+time $BWA mem $REF $INPUT/${NAME}.$TAIL1 $INPUT/${NAME}.$TAIL2 | samtools sort -o  $OUTPUT/${NAME}.bam
