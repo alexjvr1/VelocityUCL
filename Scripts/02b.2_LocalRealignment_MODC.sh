d84510cad34cfb22d327c7e5e3327ffce305d484
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-12-02 13:17:50 +0000

    Update 02b.2_LocalRealignment_MODC.sh

--- Scripts/02b.2_LocalRealignment_MODC.sh
+++ Scripts/02b.2_LocalRealignment_MODC.sh
@@ -31,14 +31,14 @@ NAME=$(sed "${SGE_TASK_ID}q;d" modc.names)
 
 
 # Identify targets to realign
-java -jar $GenomeAnalysisTK -T RealignerTargetCreator \
+java -xmx6Gg -xms6g -jar $GenomeAnalysisTK -T RealignerTargetCreator \
 -R $REF \
 -o $OUTPUT/${NAME}.intervals \
 -I $INPUT/${NAME}.rmdup.bam
 
 
 # use IndelRealigner to realign the regions found in the RealignerTargetCreator step
-java -jar $GenomeAnalysisTK -T IndelRealigner \
+java -xmx6Gg -xms6g -jar $GenomeAnalysisTK -T IndelRealigner \
 -R $REF \
 -targetIntervals $INPUT/${NAME}.intervals \
 -I $INPUT/${NAME}.rmdup.bam \
