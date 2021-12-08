eb24dde21c8d6b061cb9bd1c8160e0bdfea67a26
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-12-08 00:24:14 +0000

    Update 02b.3_ValidateSamFile_MODC.sh
    Reduce RAM requested for Java
    
    Redirect tmpdir to scratch space. Add request to java

--- Scripts/02b.3_ValidateSamFile_MODC.sh
+++ Scripts/02b.3_ValidateSamFile_MODC.sh
@@ -4,6 +4,7 @@
 #$ -l tmem=16G #RAM
 #$ -l h_vmem=16G #enforced limit on shell memory usage
 #$ -l h_rt=1:00:00 ##wall time.  
+#$ -l tscratch=10G
 #$ -j y  #concatenates error and output files (with prefix job1)
 #$ -t 1-38
 
@@ -18,6 +19,7 @@ PICARD=/share/apps/genomics/picard-2.20.3/bin/picard.jar
 
 
 #Define variables
+USERNAME=ajansen
 SHAREDFOLDER=/SAN/ugi/LepGenomics
 SPECIES=E3_Aphantopus_hyperantus
 REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
@@ -29,13 +31,26 @@ TAIL="realn.bam"
 #ls *bam | awk -F "." '{print $1}' >> modc.names
 NAME=$(sed "${SGE_TASK_ID}q;d" modc.names)
 
-echo "java -Xmx6g -Xms6g -jar $PICARD ValidateSamFile \
+#Set up	scratch	space
+mkdir -p /scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID
+TMP_DIR=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID
+TMPDIR=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID
+
+
+echo "java -Xmx4g -Xms4g -Djava.io.tmpdir=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID -jar $PICARD ValidateSamFile \
 INPUT=$INPUT/${NAME}.realn.bam \
 OUTPUT=$OUTPUT/${NAME}.validatesam
 MODE=SUMMARY" >> 02b.3_ValidateSamFile.log
 
 
-time java -Xmx6g -Xms6g -jar $PICARD ValidateSamFile \
+time java -Xmx4g -Xms4g -Djava.io.tmpdir=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID -jar $PICARD ValidateSamFile \
 INPUT=$INPUT/${NAME}.realn.bam \
 OUTPUT=$OUTPUT/${NAME}.validatesam \
 MODE=SUMMARY
+
+
+function finish {
+    rm -rf /scratch0/ajansen/$JOB_ID.$SGE_TASK_ID
+}
+
+trap finish EXIT ERR INT TERM
