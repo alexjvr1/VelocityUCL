caf4733429d8058e0f1244982030390d03cda90c
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-12-08 00:21:12 +0000

    Update 02b.2_LocalRealignment_MODC.sh

--- Scripts/02b.2_LocalRealignment_MODC.sh
+++ Scripts/02b.2_LocalRealignment_MODC.sh
@@ -3,7 +3,8 @@
 #$ -N E3.MODC.LocalRealignment  ##job name
 #$ -l tmem=16G #RAM
 #$ -l h_vmem=16G #enforced limit on shell memory usage
-#$ -l h_rt=1:00:00 ##wall time.  
+#$ -l h_rt=1:00:00 ##wall time.
+#$ -l tscratch=20G
 #$ -j y  #concatenates error and output files (with prefix job1)
 #$ -t 1-38
 
@@ -16,6 +17,7 @@ export LD_LIBRARY_PATH=/share/apps/jdk1.8.0_131/lib:$LD_LIBRARY_PATH
 
 
 # Define variables
+USERNAME=ajansen
 SHAREDFOLDER=/SAN/ugi/LepGenomics
 SPECIES=E3_Aphantopus_hyperantus
 REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
@@ -30,16 +32,29 @@ GenomeAnalysisTK=/share/apps/genomics/GenomeAnalysisTK-3.8.1.0/GenomeAnalysisTK.
 NAME=$(sed "${SGE_TASK_ID}q;d" modc.names)
 
 
+#Set up	scratch	space
+mkdir -p /scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID
+TMP_DIR=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID
+TMPDIR=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID
+
+
 # Identify targets to realign
-java -Xmx6g -Xms6g -jar $GenomeAnalysisTK -T RealignerTargetCreator \
+java -Xmx4g -Xms4g -Djava.io.tmpdir=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID -jar $GenomeAnalysisTK -T RealignerTargetCreator \
 -R $REF \
 -o $OUTPUT/${NAME}.intervals \
 -I $INPUT/${NAME}.rmdup.bam
 
 
 # use IndelRealigner to realign the regions found in the RealignerTargetCreator step
-java -Xmx6g -Xms6g -jar $GenomeAnalysisTK -T IndelRealigner \
+java -Xmx4g -Xms4g -Djava.io.tmpdir=/scratch0/$USERNAME/$JOB_ID.$SGE_TASK_ID -jar $GenomeAnalysisTK -T IndelRealigner \
 -R $REF \
 -targetIntervals $INPUT/${NAME}.intervals \
 -I $INPUT/${NAME}.rmdup.bam \
 -o $OUTPUT/${NAME}.realn.bam
+
+
+function finish {
+    rm -rf /scratch0/ajansen/$JOB_ID.$SGE_TASK_ID
+}
+
+trap finish EXIT ERR INT TERM
