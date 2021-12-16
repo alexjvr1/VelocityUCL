0a26cb50dbeaf5e990873a44629df7b4262d2262
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2021-12-16 14:33:30 +0000

    Create MajorMinor.sh

---
+++ Scripts/MajorMinor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N E3.MODC.MajMin  ##job name
+#$ -l tmem=32G #RAM
+#$ -l h_vmem=32G #enforced limit on shell memory usage
+#$ -l h_rt=20:00:00 ##wall time.
+#$ -j y  #concatenates error and output files (with prefix job1)
+
+
+
+#Run on working directory
+cd $SGE_O_WORKDIR 
+
+
+#Path to software
+export LD_LIBRARY_PATH=/share/apps/openblas-0.3.6/lib:/share/apps/armadillo-9.100.5/lib64:$LD_LIBRARY_PATH
+ATLAS=/share/apps/genomics/atlas-0.9/atlas
+
+
+#Define variables
+SHAREDFOLDER=/SAN/ugi/LepGenomics
+SPECIES=E3_Aphantopus_hyperantus
+FOLDER=E3_SubsetTests
+REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
+INPUT=$SHAREDFOLDER/$SPECIES/04_ATLAS/MODC
+OUTPUT=$SHAREDFOLDER/$SPECIES/04_ATLAS/MODC
+GLFLIST=modc.glflist
+TASK=majorMinor
+
+
+#Run analysis
+time $ATLAS task=$TASK glf=$INPUT/$GLFLIST
