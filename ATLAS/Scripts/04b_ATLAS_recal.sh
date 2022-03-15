a60f1fc526bf802a34697c0a505c64c2c509cab7
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-03-15 15:14:16 +0000

    Update 04b_ATLAS_recal.sh

--- ATLAS/Scripts/04b_ATLAS_recal.sh
+++ ATLAS/Scripts/04b_ATLAS_recal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -N E3.MODE.ATLAS.recal  ##job name
+#$ -N E3.MUS.RG1recal.CHRSonly  ##job name
 #$ -l tmem=64G #RAM
 #$ -l h_vmem=64G #enforced limit on shell memory usage
-#$ -l h_rt=1:00:00 ##wall time.
+#$ -l h_rt=24:00:00 ##wall time.
 #$ -j y  #concatenates error and output files (with prefix job1)
 
 
@@ -12,18 +12,19 @@ cd $SGE_O_WORKDIR
 
 
 #Path to software
-export LD_LIBRARY_PATH=/share/apps/openblas-0.3.19/lib:/share/apps/armadillo-9.100.5/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/share/apps/openblas-0.3.6/lib:/share/apps/armadillo-9.100.5/lib64:$LD_LIBRARY_PATH
 ATLAS=/share/apps/genomics/atlas-0.9/atlas
 
 
 #Define variables
-SHAREDFOLDER=/SAN/ugi/LepGenomics
-SPECIES=E3_Aphantopus_hyperantus
-REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCA_902806685.1_iAphHyp1.1_genomic.fna
-INPUT=$SHAREDFOLDER/$SPECIES/04_ATLAS/MODE/recal.bamlist
-OUTPUT=$SHAREDFOLDER/$SPECIES/04_ATLAS/MODE
 TASK=recal
+BAM=RG1.merged.bam
+#PMDfile only required for Museum samples
+PMD=MUS_Empiric.txt   
+INVARIANT=A.hyperantus_LR76only.bed
+POOLRGS=mergeTheseRGs.txt
+RG=RG1
 
+#Script
 
-#Run analysis
-while IFS=  read -r line; do time $ATLAS task=$TASK bam=$line region=../A.hyperantus_LR76only.bam minDepth=2  maxDepth=4 limitWindows=2 verbose > $line.recal -l avx2=yes; done < $INPUT
+time $ATLAS task=$TASK bam=$BAM pmdFile=$PMD regions=$INVARIANT minDepth=2 poolReadGroups=$POOLRGS >> $RG_recal_full.txt
