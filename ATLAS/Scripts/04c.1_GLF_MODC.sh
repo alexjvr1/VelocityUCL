425a9a51278a6d761130b6fc4778d4d4ce68dc05
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2023-02-22 15:23:12 +0200

    Create 04c.1_GLF_MODC.sh

---
+++ ATLAS/Scripts/04c.1_GLF_MODC.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N ESP.MODC.GLF  ##job name
+#$ -l tmem=16G #RAM
+#$ -l h_vmem=16G #enforced limit on shell memory usage
+#$ -l h_rt=3:00:00 ##wall time.
+#$ -j y  #concatenates error and output files (with prefix job1)
+#$ -t 1-35
+
+
+#Run on working directory
+cd $SGE_O_WORKDIR 
+
+
+#Path to software
+export LD_LIBRARY_PATH=/share/apps/openblas-0.3.6/lib:/share/apps/armadillo-9.100.5/lib64:$LD_LIBRARY_PATH
+ATLAS=/share/apps/genomics/atlas-0.9/atlas
+
+
+#Define variables
+SHAREDFOLDER=/SAN/ugi/LepGenomics
+SPECIES=Spanish_Pararge_aegeria
+REF=$SHAREDFOLDER/$SPECIES/RefGenome/GCF_905163445.1_ilParAegt1.1_genomic.fna
+INPUT=$SHAREDFOLDER/$SPECIES/02a_mapped/FINALBAMS/SplitMerge
+OUTPUT=$SHAREDFOLDER/$SPECIES/04_ATLAS/
+RECAL=$SHAREDFOLDER/$SPECIES/02a_mapped/FINALBAMS/SplitMerge/recalFiles
+TASK=GLF
+
+NAME=$(sed "${SGE_TASK_ID}q;d" bamlist)
+
+#Run analysis
+$ATLAS task=$TASK bam=$INPUT/${NAME}.bam recal=$RECAL/${NAME}_recalibrationEM.txt
