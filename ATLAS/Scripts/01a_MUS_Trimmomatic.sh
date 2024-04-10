2781d5e3b428439ad232337725b69eb64c104a4e
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2024-04-10 09:20:56 +0100

    Update 01a_MUS_Trimmomatic.sh
    Add an example of the Trimmomatic command that is generated with this script

--- ATLAS/Scripts/01a_MUS_Trimmomatic.sh
+++ ATLAS/Scripts/01a_MUS_Trimmomatic.sh
@@ -16,3 +16,10 @@
 -n 1 -t 8 -m 16 -ph 33 -c 150 -hc 0 \
 -ad /SAN/ugi/LepGenomics/Software/Trimmomatic-0.39/adapters/Velocity.fa \
 -illclip '2:30:8:1:true' -le 20 -tr 20 -sw '4:20' -minl 20 -avg 20;
+
+#This generates a Trimmomatic input per sample like this: 
+#java -jar ../Software/Trimmomatic-0.39/trimmomatic-0.39.jar PE \
+#-trimlog TrimmomaticTest/trim.log 00_raw_reads_museum_FINAL/Sample1_R1.fastq.gz \
+#00_raw_reads_museum_FINAL/Sample1_R2.fastq.gz \
+#-baseout TrimmomaticTest/Sample1.trimtest \
+#ILLUMINACLIP:../Software/Trimmomatic-0.39/adapters/TruSeq3-PE-2.fa:2:30:8:1:True LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:20 AVGQUAL:20
